db4edb46360d02aa013585a9489e04d04043c280
Goncalo Mira <goncalomira99@gmail.com>
2020-06-12 19:24:17 +0100

    Logo Done

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Logo.css";
+
+export const Logo = () => {
+  return (
+    <div className="logo-body">
+      <div className="logo-eye-line">
+        <div className="logo-eye left"></div>
+        <div className="logo-eye right"></div>
+      </div>
+    </div>
+  );
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Navbar.css";
+import { Logo } from "./Logo";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="logo"></div>
+      <div className="logo">
+        <Logo/>
+        <div className="logo-title">Gonçalo Beirão de Mira</div>
+      </div>
       <div className="links">
           <div className="link">About</div>
           <div className="link">Skills</div>
